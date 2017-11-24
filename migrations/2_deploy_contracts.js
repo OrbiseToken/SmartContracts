12b02942e1b60434ed29564c648e92652d6d595b
ArnaudovSt <telerikacademy2016@gmail.com>
2017-11-24 17:06:55 +0200

    added pausable modifier

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ var Ledger = artifacts.require("../contracts/Ledger.sol");
 var EventsLog = artifacts.require("../contracts/EventsLog.sol");
 var SafeMath = artifacts.require("../contracts/common/SafeMath.sol");
 var Ownable = artifacts.require("../contracts/modifiers/Ownable.sol");
+var Pausable = artifacts.require("../contracts/modifiers/Pausable.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Ledger);
     deployer.deploy(EventsLog);
-    deployer.deploy(SafeMath);
+    deployer.deploy(SafeMath); // one should link the library to the contract, in order to use it.
     deployer.deploy(Ownable);
+    deployer.deploy(Pausable);
 };
