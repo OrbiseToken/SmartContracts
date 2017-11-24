1eee845f0b20bff6b261fae1c8ae5bdbf4cbf8ca
ArnaudovSt <telerikacademy2016@gmail.com>
2017-11-24 17:00:15 +0200

    added ownable modifier

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var Ledger = artifacts.require("../contracts/Ledger.sol");
 var EventsLog = artifacts.require("../contracts/EventsLog.sol");
 var SafeMath = artifacts.require("../contracts/common/SafeMath.sol");
+var Ownable = artifacts.require("../contracts/modifiers/Ownable.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Ledger);
     deployer.deploy(EventsLog);
     deployer.deploy(SafeMath);
+    deployer.deploy(Ownable);
 };
