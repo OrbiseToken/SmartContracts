e63f6382ae7cc73926d4e91d9963f3f370f122f4
ArnaudovSt <telerikacademy2016@gmail.com>
2017-11-24 16:37:05 +0200

    added Safe Math library

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var Ledger = artifacts.require("../contracts/Ledger.sol");
 var EventsLog = artifacts.require("../contracts/EventsLog.sol");
+var SafeMath = artifacts.require("../contracts/common/SafeMath.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Ledger);
     deployer.deploy(EventsLog);
+    deployer.deploy(SafeMath);
 };
--- truffle.js
+++ truffle.js
@@ -1,10 +1,16 @@
 module.exports = {
   networks: {
+    local: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 4712388,
+    },
     development: {
       host: "192.168.1.106",
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4712388
     }
-}
+  }
 };
